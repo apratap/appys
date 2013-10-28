2ec0d664cbfcf72dad68741eb45f327f8c288f49
apratap <abhishek.vit@gmail.com>
2013-10-28 14:19:21 -0700

    under dev

--- src/bam.py
+++ src/bam.py
@@ -2,14 +2,10 @@
 
 
 
-import pysam
 import os
 import sys
-import re
-import pickle
-import itertools
-import collections
-import math
+import argparse
+
 
 
 ##updating the library look up path
@@ -17,73 +13,54 @@ dir = os.path.dirname(__file__)
 sys.path.append(os.path.join(dir,'../lib'));
 import bamUtils
 import utils
-
-
-#bed_file = sys.argv[1]
-#bam_files = sys.argv[2:]
-#bamUtils.feature_cov_in_bam(bed_file,bam_files,stranded=True,min_map_qual=0,lib_protocol='second-strand')
-
-#chr_size_file = sys.argv[2]
-
-
-bam_file = sys.argv[1]
-#split bam assuming input is small RNA second stranded library
-#bamUtils.split_RNASeq_reads_bam_by_strand(bam_file,library_protocol='first-strand')
-
-
-#bam_file = sys.argv[1]
-#creating the bedgraph file
-bamUtils.bam_to_bedGraph(bam_file)
-
-
-#bamUtils.create_detailed_insert_size_plot(bam_file)
-#bamUtils.plot_bamCoverage(bam_file)
+import parallel
 
 
 
+translator_userArg_to_funcName = {
+                                    'toBigWig'  : 'bam_to_bigWig',
+                                    'getStats'   : 'get_bamStats'
+                                }
 
 
 
-#
-#bamFile_name_sorted = bamUtils.name_sort_bam(bam_file)
-#(bamfile_handle,bamfile_prefix) = bamUtils.get_apt_file_handle_for_mapped_file(bamFile_name_sorted)
-#
-#bag = collections.Counter()
-#insert_size = []
-#
-#for counter,read1,read2 in itertools.izip(itertools.count(1),bamfile_handle,bamfile_handle):
-#    
-#    if counter % 1000 == 0:
-#        #print bag
-#        #print insert_size
-#        pass
-#    
-#    if not bamUtils.is_a_sequencing_readPair(read1, read2):
-#        continue
-#        pass
-#    
-#    #insert_size.append(abs(read1.isize))
-#    
-#    bag[bamUtils.get_readPair_type(read1,read2)] += 1
-#    
-#
-#print bag    
-#    
+def main():
+    SUB = 'main'
+    description  =  """
+                    ######
+                    Script to analyze BAMs
+                    ######
+                    """
+    
+    available_functions  = 'Functions available: \n'
+    for count,funcs in enumerate(translator_userArg_to_funcName.keys()):
+        available_functions +='%d: %s\n' % (count+1,funcs)
+    description += '\n\n' + available_functions    
+    
+    #user command line args processing
+    parser = argparse.ArgumentParser(description=description,add_help=True)
+    parser.add_argument('-b','--bams',dest='bam_files',nargs='+',default=None,metavar='',help='list of bam/sam files')
+    parser.add_argument('-nc','--num_cores',dest='num_cores',default=1,type=int,metavar='',help='number of slots to use')
+    parser.add_argument('-funcs', '--run_funcs',dest='run_funcs',nargs='+',default=None,metavar='',help='list of progs to run')
+    user_args = parser.parse_args()
+    
+     
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+    
+    
+    for run_func in user_args.run_funcs:
+        func_to_run = translator_userArg_to_funcName.get(run_func,None)
+        func_to_run = eval('bamUtils.%s' % func_to_run)
+        bam_stats = parallel.parallelize_func_singe_input(func_to_run,user_args.bam_files,num_cores=user_args.num_cores)
     
 
+    print bam_stats
 
-
-
-
-#generate AlignStats
-#bamUtils.bamStats(bam_file,force_run=True)
-
-
-#generate the bam coverage
-#bamUtils.plot_bamCoverage(bam_file)
-
-
-
+if __name__ == "__main__":
+    main()
+    
 
 
 
