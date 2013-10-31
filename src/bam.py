9494f7ead714f79e7c52e6d2d0deba5a7c64504a
apratap <abhishek.vit@gmail.com>
2013-10-31 14:07:32 -0700

    more dev changes to make get_bamStats robust

--- src/bam.py
+++ src/bam.py
@@ -5,6 +5,8 @@
 import os
 import sys
 import argparse
+import functools
+import pandas 
 
 
 
@@ -42,6 +44,8 @@ def main():
     parser.add_argument('-b','--bams',dest='bam_files',nargs='+',default=None,metavar='',help='list of bam/sam files')
     parser.add_argument('-nc','--num_cores',dest='num_cores',default=1,type=int,metavar='',help='number of slots to use')
     parser.add_argument('-funcs', '--run_funcs',dest='run_funcs',nargs='+',default=None,metavar='',help='list of progs to run')
+    parser.add_argument('-f', '--force_run',dest='force_run',default=False,action='store_true',help='list of progs to run')
+    parser.add_argument('-o', '--output_dir',dest='output_dir',default=None,help='dir to place the output in..<default: same as input file dir>')
     user_args = parser.parse_args()
     
      
@@ -49,15 +53,30 @@ def main():
         parser.print_help()
         sys.exit()
     
+    #create output dir if not exists
+    if user_args.output_dir:
+        output_dir = os.path.abspath(user_args.output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+    
+    
     
     for run_func in user_args.run_funcs:
         func_to_run = translator_userArg_to_funcName.get(run_func,None)
         func_to_run = eval('bamUtils.%s' % func_to_run)
-        bam_stats = parallel.parallelize_func_singe_input(func_to_run,user_args.bam_files,num_cores=user_args.num_cores)
+        #create a partial func
+        func_to_run = functools.partial(func_to_run,force=user_args.force_run,output_dir=output_dir)
+        result = parallel.parallelize_func_singe_input(func_to_run,user_args.bam_files,num_cores=user_args.num_cores)
     
 
-    print bam_stats
-
+    
+    #TO BE MADE MORE ROBUST
+    #will error if there is any func arg other than 'getStats'
+    df = pandas.DataFrame.from_dict(result)
+    df = df.set_index(['bamFile'])
+    print df
+    
 if __name__ == "__main__":
     main()
     
